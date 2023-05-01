a4a6e518d9aa7842309929c478a61c1d830f80b1
!        yVoidzin 性 <gabrielhenriquelinssxw@gmail.com>
2023-05-01 11:44:58 -0300

    Update removeEmojis.js

--- src/Functions/removeEmojis.js
+++ src/Functions/removeEmojis.js
@@ -1,6 +1,6 @@
 module.exports = function removeEmojis(str) {
     if(!str) return new Error('[ 🔧 ] - Parameter <str> not defined!');
     if(typeof str !== 'string') return new Error('[ 🔧 ] - Parameter <str-string> not set!'); 
-    const emojiPattern = /[\u{1F300}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F900}-\u{1F9FF}\u{1F1E0}-\u{1F1FF}]/gu;
+    const emojiPattern = /(?:<:[^:]+:\d+>)|(?:[\u2000-\u3300\uDC00-\uDFFF\s\uD83C-\uD83E][\uDC00-\uDFFF\s\uD83C-\uD83E]*?)+|™/g;
     return str.replace(emojiPattern, '').trim();
 }
