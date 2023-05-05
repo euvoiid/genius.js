c4e0c891966e38fe60984c97f711519bdc02911e
!        yVoidzin 性 <gabrielhenriquelinssxw@gmail.com>
2023-05-05 20:55:33 -0300

    .

--- readme.md
+++ readme.md
@@ -35,7 +35,8 @@
 - `- removeDuplicatesArr`
 - `- removeEmojis`
 - `- shorten`
-- `- symbolFormat`
+- `- abbreviate`
+- `- unabbreviate`
 - `- timeToMs`
 - `- toMorseCode`
 
--- src/Functions/unabbreviate.js
+++ src/Functions/unabbreviate.js
@@ -1,24 +1,22 @@
 module.exports = function unabbreviate(abbreviate) {
-    if(!abbreviate) return new Error('[ 🔧 ] - Parameter <abbreviate> not defined!');
+  if(!abbreviate) return new Error('[ :wrench: ] - Parameter <abbreviate> not defined!');
 
-    const Symbol = [
-      {
-        k: 1000, // Mil
-        m: 1000000, // Milhão
-        b: 1000000000, // Bilhão 
-        t: 1000000000000, // Trilhão
-        K: 1000, 
-        M: 1000000, 
-        B: 1000000000, 
-        T: 1000000000000
-      }
-    ];
-  
-    const un = abbreviate.slice(-1);
-  
-    const valueAbbreviate = parseFloat(abbreviate);
-  
-    const valueComplet = valueAbbreviate * Symbol[un];
-  
-    return valueComplet;
+  const Symbol = {
+      k: 1000, // Mil
+      m: 1000000, // Milhão
+      b: 1000000000, // Bilhão 
+      t: 1000000000000, // Trilhão
+      K: 1000, // Mil
+      M: 1000000, // Milhão
+      B: 1000000000,  // Bilhão 
+      T: 1000000000000, // Trilhão
+    };
+
+  const un = abbreviate.slice(-1) || 0;
+
+  const valueAbbreviate = parseFloat(abbreviate);
+
+  const valueComplet = valueAbbreviate * Symbol[un];
+
+  return valueComplet;
 }
